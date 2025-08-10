d5e7331f98376491bc0b6d8b9074bec146b4e630
awfixer <git@awfixer.me>
2025-08-10 03:16:11 -0700

    update .gitignore

--- tina/config.tsx
+++ tina/config.tsx
@@ -10,7 +10,7 @@ import Tag from "./collection/tag";
 const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || master
+    process.env.NEXT_PUBLIC_TINA_BRANCH! || master,
   token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this
